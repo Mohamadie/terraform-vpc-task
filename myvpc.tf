676c8271c61532bdcea729be8451e25628b2ff73
Mohamadie <95703432+Mohamadie@users.noreply.github.com>
2023-01-30 23:19:24 -0600

    Update myvpc.tf

--- myvpc.tf
+++ myvpc.tf
@@ -1,19 +1,15 @@
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "AWS Region"
-}
-
 module "vpc" {
-  source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
-  vpc_tags = {
-    Team = var.team_name
-  }
-  subnet1_cidr_block = "10.0.0.0/24"
-  subnet1_az         = "${var.region}a"
-  subnet2_cidr_block = "10.0.1.0/24"
-  subnet2_az         = "${var.region}b"
-  subnet3_cidr_block = "10.0.2.0/24"
-  subnet3_az         = "${var.region}c"
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = var.tags
 }
